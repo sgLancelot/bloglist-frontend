e549c5ac07fdd6c4b61b3081c2c7c3f44e83be07
tzhenkai <sglancelot@gmail.com>
2019-10-05 17:42:07 +0800

    Completed part 5a qn 5.1. Conditional redering and logging in

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import blogService from './services/blogs'
+import loginService from './services/login'
+import Blog from './components/Blog'
 
-function App() {
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService
+      .getAll()
+      .then(initialBlogs => {
+        setBlogs(initialBlogs)
+      })
+  }, [])
+
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log('wrong credentials')
+    }
+  }
+
+  const loginForm = () => {
+    return (
+      <div>
+        <h1>log in to application</h1>
+        <form onSubmit={handleLogin}>
+          <div>
+              username 
+                <input type='text' 
+                value={username} 
+                name="Username" 
+                onChange={({target}) => setUsername(target.value)}
+                />
+            </div>
+            <div>
+              password 
+                <input type='text'
+                value={password}
+                name='Password'
+                onChange={({target}) => setPassword(target.value)}
+                />
+            </div>
+            <button type='submit'>login</button>
+        </form>
+      </div>
+    )
+  }
+
+  const blogForm = () => {
+    return (
+      <div>
+        <h2>{user.name} logged in</h2>
+        {blogs
+          .filter(x => x.user.name===user.name)
+          .map(x => {
+            return (
+              <div key={x.id}>
+                <Blog blog={x} />
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {user === null ?
+        loginForm() :
+        blogForm()
+      }
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
